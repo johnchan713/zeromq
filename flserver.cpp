835ae91f269946738475dee2bd13f634d1b6f920
johnchan713 <johnchan713@gmail.com>
2014-04-01 12:10:33 +0800

    freelance model 2 in C++ (WIP)

--- flserver.cpp
+++ flserver.cpp
@@ -1,33 +1,42 @@
-//-----------------ZMQ_freelance1_SERVER---------------
+//-----------------ZMQ_freelance2_SERVER---------------
 #include <zmq.hpp>
 #include <zmsg.hpp>
 #include <stdio.h>
 #include <iostream>
-#include <sstream>
 
 int main(int argc, char *argv[])
 {
-	if (argc <2) {
-	std::cout << "Error: syntax should be " << argv[0] << " <endpoint> \n";	
+    if (argc < 2) {
+	std::
+	    cout << "Error: syntax should be " << argv[0] <<
+	    " <endpoint> \n";
 	return 0;
-	}
+    }
 
-    std::cout << "creating context and socket..\n";
-    zmq::context_t ctx(1);
-    zmq::socket_t socket(ctx, ZMQ_REP);
-
-    std::cout << "binding to " << argv[1] << "... \n";
-    socket.bind(argv[1]);
+    zmq::context_t * ctx = new zmq::context_t(1);
+    zmq::socket_t * server = new zmq::socket_t(*ctx, ZMQ_REP);
+    server->bind(argv[1]);
 
     std::cout << "Service is ready at " << argv[1] << std::endl;
     while (1) {
-	zmsg zm (socket);
+	zmsg *msg = new zmsg(*server);
+	if (!msg)
+	    break;
 
-	std::cout << "Reply received - " << zm.body () << std::endl;
-	zm.send(socket);
-	std::cout << "response has been sent to client!\n";
+	assert(msg->parts() == 3);
+
+	char *identity = (char *) (*msg).pop_front().c_str();
+	msg->clear();
+
+	zmsg *reply = new zmsg();
+	reply->body_set(identity);
+	reply->push_front("OK");
+	reply->send(*server);
     }
-	
-    ctx.close();
+
+    if (s_interrupted)
+	std::cout << "Interrupted \n";
+
+    ctx->close();
     return 0;
 }
